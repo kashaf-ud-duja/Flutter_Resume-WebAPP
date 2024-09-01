641c03b0aa2452d12bac5848fe00851f0a62d5c6
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-09-01 18:02:26 +0500

    Animated Cursor

--- lib/Widgets/Cursor/animated_circle_cursor.dart
+++ lib/Widgets/Cursor/animated_circle_cursor.dart
@@ -2,7 +2,7 @@ import 'package:cv/Widgets/Cursor/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:provider/provider.dart';
 
 class AnimatedCursorState {
   final BoxDecoration? decoration;
@@ -12,42 +12,73 @@ class AnimatedCursorState {
   final Size size1;
   final Size size2;
 
-AnimatedCursorState({
-this.decoration,
-this.offset1 = Offset.zero,
-this.offset2 = Offset.zero,
-this.size1 = circle1Size,
-this.size2 = circle2Size2,
-});
+  AnimatedCursorState({
+    this.decoration,
+    this.offset1 = Offset.zero,
+    this.offset2 = Offset.zero,
+    this.size1 = circle1Size,
+    this.size2 = circle2Size2,
+  });
 
-static const Size circle1Size = Size(30, 30);
-static const Size circle2Size2 = Size(5, 5);
-
-
+  static const Size circle1Size = Size(30, 30);
+  static const Size circle2Size2 = Size(5, 5);
 }
 
+class AnimatedCursorProvidor extends ChangeNotifier {
+  AnimatedCursorProvidor();
+  AnimatedCursorState state = AnimatedCursorState();
+  void changeCursor(
+      {required GlobalKey key, required BoxDecoration BoxDecoration}) {
+    final renderBox = key.currentContext?.findRenderObject() as RenderBox;
+    if (renderBox == null) {
+      return;
+    } else {
+      state = AnimatedCursorState(
+          size1: Size(60, 60),
+          offset1: state.offset1,
+          decoration: CursorConstants.boxdecorationhovered);
+      notifyListeners();
+    }
+  }
 
+  void resetCursor() {
+    state = AnimatedCursorState();
+    notifyListeners();
+  }
 
-class AnimatedCursorProvidor extends ChangeNotifier{
-AnimatedCursorProvidor();
-AnimatedCursorState state = AnimatedCursorState();
-void changeCursor({required GlobalKey key , required BoxDecoration BoxDecoration}){
-final renderBox = key.currentContext?.findRenderObject() as RenderBox;
-if(renderBox == null){
-  return;
-}else{
-
-  state = AnimatedCursorState(size1:Size(60, 60) ,offset1: state.offset1,decoration: CursorConstants.boxdecorationhovered );
+  void updateCursorPosition(Offset position) {
+    state = AnimatedCursorState(offset1: position, offset2: position);
+    notifyListeners();
+  }
 }
-}
 
-void resetCursor(){
-state = AnimatedCursorState();
-}
+class AnimatedCircleCursor extends StatelessWidget {
+  const AnimatedCircleCursor({super.key, required this.child});
 
+  final Widget child;
 
-void updateCursorPosition(Offset position){
-state = AnimatedCursorState(offset1: position, offset2: position );
-}
-
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AnimatedCursorProvidor(),
+      child: Consumer<AnimatedCursorProvidor>(
+        child: child,
+        builder: (context, value, child) {
+          final state = value.state;
+          return Stack(
+            children: [
+              child != null ? child : Container(),
+              Positioned.fill(
+                child: MouseRegion(
+                  opaque: false,
+                  onHover: (event) => context.read<AnimatedCursorProvidor>().updateCursorPosition(event.position),
+                ),
+              ),
+              
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cv/Views/homepage.dart';
+import 'package:cv/Widgets/Cursor/animated_circle_cursor.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnimatedCircleCursor(
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
